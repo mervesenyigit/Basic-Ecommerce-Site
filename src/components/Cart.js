f95cb1dba58470d3289e7a98f322aae7c959ab3b
mervesnygt <merve.senyigit@outlook.com>
2025-01-24 17:55:17 +0300

    Enhanced design with product table, improved image display, and better spacing.

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,5 +1,7 @@
 import React, { useContext } from "react";
 import { useCart } from "./CartContext";
+import "./Cart.css";
+
 
 const Cart = () => {
 
@@ -8,24 +10,33 @@ const Cart = () => {
   return (
    
     <div className="container d-flex flex-column align-items-center">
-      <h1 className="d-block">Shopping Cart</h1>
-      <div className=" ">
-      {cart.length === 0 ? (
         
+        <h1 className="text-center">Shopping Cart</h1>
+
+      {cart.length === 0 ? (
         <p>Your cart is empty.</p>
       ) : (
-        <ul>
-          {cart.map((item, index) => (
-            <div key={index}>
-              <img src={item.image} alt={item.name} style={{ width: "50px" }} />
-              <span>{item.name} - {item.price} TL</span>
-            </div>
-          ))}
-        </ul>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>Image</th>
+              <th>Product Name</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cart.map((item, index) => (
+              <tr key={index}>
+                <td>
+                  <img src={item.image} alt={item.name} style={{ width: "50px" }} />
+                </td>
+                <td>{item.name}</td>
+                <td>{item.price} TL</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       )}
-      </div>
-     
-   
     </div>
   );
 };
